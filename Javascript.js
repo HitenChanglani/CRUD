0678fc5d5b21da53edafd82a7e9d4df19b3fc5d2
Hiten Changlani <changlanihiten@gmail.com>
2019-12-03 18:46:21 +0530

    ABC

--- Javascript.js
+++ Javascript.js
@@ -3,19 +3,81 @@ let Data = [];
 
 function OnClickRead(){
     "use strict";
+    let a, v1, v2, v3, v4, v5, v6, v7, v8;
+
     let entry = {
-    fName: document.getElementById("FirstName").value,
-    lName: document.getElementById("LastName").value,
-    con: document.getElementById("Contact").value,
-    TaxID: document.getElementById("TaxID").value,
-    gender: gender(),
-    uName: document.getElementById("Username").value,
-    email: document.getElementById("Email").value,
-    password: document.getElementById("Pass").value
+        fName: document.getElementById("FirstName").value,
+        lName: document.getElementById("LastName").value,
+        con: document.getElementById("Contact").value,
+        TaxID: document.getElementById("TaxID").value,
+        gender: gender(),
+        uName: document.getElementById("Username").value,
+        email: document.getElementById("Email").value,
+
     }
-    Data.push([index, entry]);
-    index = index + 1;
-    console.log(entry);
+    let confpwd = document.getElementById("cnf").value;
+    let password = document.getElementById("Pass").value;
+
+    if(entry.fName == ""){
+        v1 = false;
+        alert("Please enter a valid First Name!");
+    }
+    else{ v1 = true;}
+
+    if(entry.lName == ""){
+        v2 = false;
+        alert("Please enter a valid Last Name!");
+    }
+    else{ v2 = true;}
+
+    if(entry.con.length != 10 && typeof(entry.con) != "number"){
+        v3 = false;
+        alert("Please enter a valid contact number!");
+    }
+    else{ v3 = true;}
+
+    if(entry.TaxID.length != 12 && typeof(entry.TaxID) != "number"){
+        v4 = false;
+        alert("Please enter a valid Tax ID!");
+    }
+    else{ v4 = true;}
+
+    if(entry.gender == undefined){
+        v5 = false;
+        alert("Please select a gender!");
+    }
+    else{v5 = true;}
+
+    if(entry.uName == ""){
+        v6 = false;
+        alert("Please enter a valid Username!");
+    }
+    else{ v6 = true;}
+
+    if(entry.email == ""){
+        v7 = false;
+        alert("Please enter a valid e-mail ID!");
+    }
+    else{ v7 = true;}
+
+    if(password == "" || password != confpwd){
+        v8 = false;
+        alert("Passwords do not match!");
+    }
+    else{ v8 = true;}
+
+    a = v1 & v2 & v3 & v4 & v5 & v6 & v7 & v8;
+
+
+    if(a){
+        let x = confirm("Are you sure you want to submit?");
+        if(x){
+            Data.push([index, entry]);
+            index = index + 1;
+            console.log(entry);
+        }
+    }    
+
 }
 
 function display(){
@@ -38,22 +100,22 @@ function gender(){
 }
 
 function change(){
-    let inputvalue = prompt("Please enter the index number:");
+    let inputvalue = prompt("Please enter the Username:");
     for(i = 0; i <= index; i++){
-        if(inputvalue == i){
+        if(inputvalue == Data[i][1].uName){
             let Val = prompt("Press the following numbers to update profile: 1 for Contact & 2 for E-mail");
             if(Val == 1){
                 let bool = true;
                 let Mobval;
                 while(bool){
                     MobVal = prompt("Enter 10 digit contact number:");
-                      if(MobVal.length != 10){
-                          alert("Incorrect input. Please  enter 10 digit valid mobile number!");
-                      }
+                    if(MobVal.length != 10){
+                        alert("Incorrect input. Please  enter 10 digit valid mobile number!");
+                    }
                     else if(MobVal.length == 10){
                         bool = false;
                     }
-                      }
+                }
                 Data[i][1].con = MobVal;
                 break;
             }
@@ -61,15 +123,20 @@ function change(){
                 Data[i][1].email = prompt("Enter new E-mail address:");
                 break;
             }
+            else{
+                alert("Please select the numbers mentioned in the pop-up box!");
+                break;
+            }
         }
     }
 }
 
 function DelEntry(){
-    let Delvalue = prompt("Please enter the index number:");
+    let Delvalue = prompt("Please enter the Username:");
     for(i = 0; i <= index; i++){
-        if(Delvalue == i){
+        if(Delvalue == Data[i][1].uName){
             Data.splice(i, 1);
+            break;
         }
     }
 }
